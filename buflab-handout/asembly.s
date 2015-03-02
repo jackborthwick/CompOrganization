9cf3aeb4f22a78afdfa958b7cbbae142b63940ba
Jack Borthwick <jackmckeanborthwick@gmail.com>
2015-03-01 18:24:13 -0700

    latest assignments

---
+++ buflab-handout/asembly.s
@@ -0,0 +1,5 @@
+mov		$0x556836b0,%ebp
+mov    $0x2f926fe9,%eax
+mov    $0x8048dce,%ecx
+jmp    *%ecx
+ret
---
+++ buflab-handout/dynamite.c
@@ -0,0 +1,8 @@
+unsigned dynamite( int x)
+{
+	unsigned y= 0x2f926fe9;
+if(x){
+	void *addr=0x08048dcd;		
+	goto *addr;}
+return y;
+}
